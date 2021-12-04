df5ee199158db2877173c5ecb9698adf660b63dd
lbenito94 <luisbnt101@gmail.com>
2021-12-04 18:52:16 -0500

    adecua nav, y banner de inicio

--- src/main/java/com/dental/controller/HomeController.java
+++ src/main/java/com/dental/controller/HomeController.java
@@ -3,7 +3,6 @@ package com.dental.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
-
 @Controller
 public class HomeController {
 
